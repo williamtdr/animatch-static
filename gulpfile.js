859948d6c9afbf9d9268aa3266cfdb271f94a818
William Teder <wteder@hydreon.com>
2016-01-08 11:30:02 -0600

    Rename, remove other/, markdown

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,21 @@ var gulp = require('gulp'),
 gulp.task('minify-css', function() {
 	return gulp.src('css/**/*.css')
 		.pipe(minifyCss({compatibility: 'ie8'}))
-		.pipe(concat('totem.vendor.min.css'))
+		.pipe(concat('animatch.vendor.min.css'))
 		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-js', function() {
 	return gulp.src(['js/jquery.min.js', 'js/transition.min.js', 'js/**/*.js'])
         .pipe(uglify())
-		.pipe(concat('totem.vendor.min.js'))
+		.pipe(concat('animatch.vendor.min.js'))
 		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-file-upload', function() {
     return gulp.src(['fileupload/jquery.ui.widget.js', 'fileupload/load-image.all.min.js', 'fileupload/canvas-to-blob.min.js', 'fileupload/jquery.fileupload.js', 'fileupload/jquery.fileupload-process.js', 'fileupload/jquery.fileupload-image.js', 'fileupload/jquery.fileupload-validate.js', 'fileupload/oncomplete.js'])
         .pipe(uglify())
-        .pipe(concat('totem.fileupload.min.js'))
+        .pipe(concat('animatch.fileupload.min.js'))
         .pipe(gulp.dest('dist'))
 });
 
